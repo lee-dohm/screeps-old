50e0073f0cda38b7a5a0e7b7060da1f6cb9fef43
Lee Dohm <lee@lee-dohm.com>
2016-10-09 13:56:14 -0700

    :art:

--- construct.js
+++ construct.js
@@ -1,5 +1,6 @@
 var construct = function (role, count) {
   var creeps = _.filter(Game.creeps, (creep) => creep.memory.role === role)
+
   if (creeps.length < count) {
     var newCreep = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], undefined, {role: role})
     console.log('Spawning new ' + role + ': ' + newCreep)
