72f44d42213b55415f4dd9749bdaf4d448d647f3
Lee Dohm <lee@lee-dohm.com>
2016-10-09 15:55:26 -0700

    :art:

--- monkeypatch.js
+++ monkeypatch.js
@@ -1,19 +1,20 @@
 Room.prototype.lookForAround = function (what, x, y, distance) {
-  var top = y - distance
-  var left = x - distance
-  var bottom = y + distance
-  var right = x + distance
+  let top = y - distance
+  let left = x - distance
+  let bottom = y + distance
+  let right = x + distance
 
   return this.lookForAtArea(what, top, left, bottom, right, true)
 }
 
 Room.prototype.lookForEmptyTerrainAround = function (x, y, distance) {
-  var top = y - distance
-  var left = x - distance
-  var bottom = y + distance
-  var right = x + distance
+  let top = y - distance
+  let left = x - distance
+  let bottom = y + distance
+  let right = x + distance
 
-  var spaces = this.lookAtArea(top, left, bottom, right, true)
+  let spaces = this.lookAtArea(top, left, bottom, right, true)
+
   return _.filter(spaces, (space) => {
     if (space.type == 'terrain' && this.lookAt(space.x, space.y).length == 1) {
       return true
